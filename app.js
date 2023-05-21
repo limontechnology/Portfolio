ee8f2e029cd41cbcc331912b76a8a393122adddc
David Limon <limon7echnology@gmail.com>
2023-05-21 03:09:04 -0600

    Se agrega la seccion del Blog y de Contact, se corrige error de JS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 function PageTransitions() {
 //Button click active class
@@ -9,23 +9,27 @@ function PageTransitions() {
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn'
+            this.className += ' active-btn';
         })
     }
-    //Sections Activa
+
+    //Sections Active
     allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
         if(id){
             //Remove selected from the others btns
             sectBtns.forEach((btn) => {
                 btn.classList.remove('active')
             })
             e.target.classList.add('active')
-            //hide other sections
-            sections.forEach((section)=>{
+
+            //Hide other sections
+            sections.forEach((section) => {
                 section.classList.remove('active')
             })
+
             const element = document.getElementById(id);
-            element.classList.add('active')
+            element.classList.add('active');
         }
     })
 }
